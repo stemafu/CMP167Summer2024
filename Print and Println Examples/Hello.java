5c3fa6bdfec08cb5dd291a24d77c12e0e7f4368d
Steven Fulakeza <steven.fulakeza@lehman.cuny.edu>
2024-06-04 23:26:42 -0400

    print and println examples from class

---
+++ Print and Println Examples/Hello.java
@@ -0,0 +1,28 @@
+// This is used for a one line comment
+// Author:  Steven Fulakeza
+
+/*
+ * This is an example of a multi-line comment
+ * 
+ * This computer program prints Hello World to the screen
+ */
+
+
+/*
+ * Author: Steven Fulakeza
+ * Date created: 6/03/2024
+ * Date modified:
+ * Email: steven.fulakeza@lehman.cuny.edu
+ * Description of the Program: This program prints Hello World to the screen.
+ * 
+ * Comments are great for documentation
+ */
+public class Hello {public static void main(String[] args) {
+		System.out.println("* * * *");
+		System.out.println("*     *");
+		System.out.println("*     *");
+		System.out.println("* * * *");
+
+	}
+
+}
